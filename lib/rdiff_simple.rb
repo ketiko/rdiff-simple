c31eca67617c63e20ca0901ae2cd521f312c295e
Ryan Hansen <ketiko@gmail.com>
2013-06-30 17:04:01 -0600

    Adding tests

--- lib/rdiff_simple.rb
+++ lib/rdiff_simple.rb
@@ -1,1 +1,13 @@
 require "rdiff_simple/version"
+
+module RdiffSimple
+  def self.execute(*args)
+    raise 'rdiff-backup not installed' unless installed?
+
+    system('rdiff-backup', *args)
+  end
+
+  def self.installed?
+    system('which rdiff-backup')
+  end
+end
--- spec/rdiff_simple_spec.rb
+++ spec/rdiff_simple_spec.rb
@@ -1,7 +1,46 @@
 require 'spec_helper'
 
 describe RdiffSimple do
-  it "should be defined" do
-    defined?(RdiffSimple).should == 'constant'
+  describe '#execute' do
+    context 'when no arguments are given' do
+      it 'should return false' do
+        subject.execute.should be_false
+      end
+    end
+
+    context 'when arguments are given' do
+      it 'should return true' do
+        subject.execute('--version').should be_true
+      end
+    end
+  end
+
+  describe '#installed?' do
+    def mock_rdiff_installed(value)
+      subject
+      .should_receive(:system)
+      .with('which rdiff-backup')
+      .and_return(value)
+    end
+
+    context 'when rdiff-backup is installed' do
+      before(:each) do
+        mock_rdiff_installed(true)
+      end
+
+      it 'should be true' do
+        subject.installed?.should be_true
+      end
+    end
+
+    context 'when rdiff-backup is not installed' do
+      before(:each) do
+        mock_rdiff_installed(false)
+      end
+
+      it 'should be false' do
+        subject.installed?.should be_false
+      end
+    end
   end
 end
