7a17b76adc17a57f795963f0d600916700d330a3
Ryan Hansen <ketiko@gmail.com>
2013-07-01 22:00:06 -0600

    Updating tests [skip ci]

--- spec/rdiff_simple_spec.rb
+++ spec/rdiff_simple_spec.rb
@@ -3,33 +3,33 @@ require 'spec_helper'
 describe RdiffSimple do
   describe '#execute' do
     context 'when rdiff-backup is installed' do
-      before(:each) do
+      before do
         mock_rdiff_installed(true)
       end
 
       context 'when no arguments are given' do
-        before(:each) do
-          subject.stub(:system).with('rdiff-backup').and_return(false)
+        before do
+          RdiffSimple.stub(:system).with('rdiff-backup').and_return(false)
         end
 
-        it 'should return false' do
-          subject.execute.should be_false
-        end
+        subject { RdiffSimple.execute }
+
+        it { should be_false }
       end
 
       context 'when arguments are given' do
-        before(:each) do
-          subject.stub(:system).with('rdiff-backup', '--version').and_return(true)
+        before do
+          RdiffSimple.stub(:system).with('rdiff-backup', '--version').and_return(true)
         end
 
-        it 'should return true' do
-          subject.execute('--version').should be_true
-        end
+        subject { RdiffSimple.execute('--version') }
+
+        it { should be_true }
       end
     end
 
     context 'when rdiff-backup is not installed' do
-      before(:each) do
+      before do
         mock_rdiff_installed(false)
       end
 
@@ -41,28 +41,24 @@ describe RdiffSimple do
 
   describe '#installed?' do
     context 'when rdiff-backup is installed' do
-      before(:each) do
+      before do
         mock_rdiff_installed(true)
       end
 
-      it 'should be true' do
-        subject.installed?.should be_true
-      end
+      it { should be_installed }
     end
 
     context 'when rdiff-backup is not installed' do
-      before(:each) do
+      before do
         mock_rdiff_installed(false)
       end
 
-      it 'should be false' do
-        subject.installed?.should be_false
-      end
+      it { should_not be_installed }
     end
 
   end
 
   def mock_rdiff_installed(value)
-    subject.stub(:system).with('which rdiff-backup').and_return(value)
+    RdiffSimple.stub(:system).with('which rdiff-backup').and_return(value)
   end
 end
