198efa3b809521fd5f6140a78ffd57909a8220d8
Ryan Hansen <ketiko@gmail.com>
2013-08-09 17:10:24 -0600

    Fixing stuff

--- lib/rdiff_simple/rdiff_backup.rb
+++ lib/rdiff_simple/rdiff_backup.rb
@@ -1,18 +1,20 @@
 require 'open3'
 
 module RdiffSimple
-  module RdiffBackup
-    def self.execute(*args)
+  class RdiffBackup
+    attr_reader :output_log
+
+    def execute(*args)
       raise NotInstalledError, 'rdiff-backup not installed' unless installed?
 
       command = ['rdiff-backup', args].flatten!.compact.join(' ').strip
 
-      output, status = Open3.capture2e(command)
-      STDOUT.write output
+      @output_log, status = Open3.capture2e(command)
       status == EXIT_CODE[:success]
     end
 
-    def self.installed?
+    private
+    def installed?
       output, status = Open3.capture2e(COMMANDS[:installed])
       status == EXIT_CODE[:success]
     end
