0f1393dbbff6be7e81bf7deb1dc617446ec1ac84
Ryan Hansen <ketiko@gmail.com>
2013-10-20 19:00:13 -0600

    Only requiring core_ext from active support

--- lib/rdiff_simple.rb
+++ lib/rdiff_simple.rb
@@ -1,6 +1,6 @@
 require 'logger'
 require 'open3'
-require 'active_support/all'
+require 'active_support/core_ext'
 require 'rdiff_simple/version'
 require 'rdiff_simple/options_parser'
 require 'rdiff_simple/rdiff_backup'
